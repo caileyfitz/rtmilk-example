f3fd50c30c50b43bb69cd2f038005c7b1757fc22
Natasha Richter <39qsw@osf.io>
2015-05-21 14:09:11 -0400

    File deleted on behalf of the Open Science Framework

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms.fields import TextField, BooleanField
-from wtforms.validators import Required
-
-class ListItem(Form):
-	additem = TextField('item', validators = [Required()])
