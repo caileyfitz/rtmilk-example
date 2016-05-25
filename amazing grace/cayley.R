d87a4c1ed54192aaf3d82c9b5563f5bb2a85683c
Cailey Fitzgerald <cef3fu@virginia.edu>
2016-05-25 17:12:26 -0400

    Deleted via the Open Science Framework

--- amazing grace/cayley.R
+++
@@ -1,7 +0,0 @@
-cayley graphs intro
-
-Cayley graphs, named after mathematician Arthur Cayley, 
-are an important concept relating group theory and graph theory. 
-Cayley graphs are frequently used to render the abstract structure 
-of a group easily visible by way of representing this structure 
-in graph form.
